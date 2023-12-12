b2fbacd546494ba38c6996dd1c688568a0182bdd
veruklimpl <veronika.klimplova@seznam.cz>
2023-12-12 18:42:14 +0100

    Projekt zaloižený pomocí start.spring.io

---
+++ src/test/java/io/github/vklimplova/kdybudouvanoce/KdyBudouVanoceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.vklimplova.kdybudouvanoce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KdyBudouVanoceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
